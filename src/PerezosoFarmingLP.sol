37aa0ab4edba147482b7e167ac46d39c90ecb249
alberthaptic <albert@haptic.finance>
2024-06-02 21:16:23 -0400

    update contracts

--- src/PerezosoFarmingLP.sol
+++ src/PerezosoFarmingLP.sol
@@ -2,88 +2,28 @@
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./RewardsDistributionRecipient.sol";
+import "./IRewardsDistributionRecipient.sol";
 import "./TokenWrapper.sol";
+import "./SafeMath.sol";
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
-    }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, ReentrancyGuard {
+contract PerezosoFarmingLP is TokenWrapper, IRewardsDistributionRecipient, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-
+    
     IERC20  public  perezoso;
-    IERC20  public stakingToken;
-
-    uint256 public  DURATION = 7 days;
+    uint256 public constant DURATION = 7 days;
+    address public deployer;
 
     uint256 public periodFinish = 0;
     uint256 public rewardRate = 0;
     uint256 public lastUpdateTime;
     uint256 public rewardPerTokenStored;
-
     mapping(address => uint256) public userRewardPerTokenPaid;
     mapping(address => uint256) public rewards;
-
-    mapping(address => Stake[]) public stakes;
-
-    uint256 private _totalSupply;
-    
-    mapping(address => uint256) private _balances;
-    mapping(address => uint256) public lastWithdrawalTime;
-
-    address public deployer;
-
-    struct Stake {
-        uint256 amount;
-    }
 
     event RewardAdded(uint256 reward);
     event Staked(address indexed user, uint256 amount);
@@ -91,23 +31,23 @@ contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, Reentr
     event RewardPaid(address indexed user, uint256 reward);
     event TokensRecovered(address token, uint256 amount);
 
-    constructor(
-        address _owner, 
-        address _stakingToken, 
-        address _rewardToken
-    ) Ownable(_owner) {
-        stakingToken = IERC20(_stakingToken);
-        perezoso = IERC20(_rewardToken); 
+    constructor(address _owner, address _stakingToken, address _rewardToken)
+    Ownable(_owner)
+    TokenWrapper(_stakingToken)
+    {
+        perezoso = IERC20(_rewardToken);
         transferOwnership(_owner);
         deployer = msg.sender;
     }
 
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view override returns (uint256) {
-        return _balances[account];
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
     }
 
     function lastTimeRewardApplicable() public view returns (uint256) {
@@ -130,48 +70,32 @@ contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, Reentr
 
     function earned(address account) public view returns (uint256) {
         return
-            _balances[account]
+            balanceOf(account)
                 .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
                 .div(1e18)
                 .add(rewards[account]);
     }
 
-
-    function earnedOnStake(address account, uint256 index) public view returns (uint256) {
-        require(index < stakes[account].length, "Stake index out of bounds");
-        Stake memory userStake = stakes[account][index];
-        uint256 earnedAmount = userStake.amount * (rewardPerToken() - userRewardPerTokenPaid[account]) / 1e18;
-        return earnedAmount;
-    }
-
-    function stakesCount(address user) public view returns (uint256) {
-        return stakes[user].length;
-    }
-
-    function getAllStakes(address user) public view returns (Stake[] memory) {
-        return stakes[user];
-    }
-
-    function stake(uint256 amount) public override updateReward(msg.sender) {
+    // stake visibility is public as overriding LPTokenWrapper's stake() function
+    function stake(uint256 amount) public override updateReward(msg.sender) nonReentrant {
         require(amount > 0, "Cannot stake 0");
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
-        stakingToken.safeTransferFrom(msg.sender, address(this), amount); 
+        super.stake(amount);
         emit Staked(msg.sender, amount);
     }
 
-    function withdraw(uint256 amount) public override updateReward(msg.sender) {
+    function withdraw(uint256 amount) public override updateReward(msg.sender) nonReentrant {
         require(amount > 0, "Cannot withdraw 0");
-
-        _totalSupply = _totalSupply.sub(amount);
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        stakingToken.safeTransfer(msg.sender, amount); 
+        super.withdraw(amount);
         emit Withdrawn(msg.sender, amount);
     }
 
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        getReward();
+    }
+
     function getReward() public updateReward(msg.sender) {
-        uint256 reward = rewards[msg.sender];
+        uint256 reward = earned(msg.sender);
         if (reward > 0) {
             rewards[msg.sender] = 0;
             perezoso.safeTransfer(msg.sender, reward);
@@ -193,7 +117,6 @@ contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, Reentr
             require(newRewardRate >= rewardRate, "New reward rate too low");
             rewardRate = newRewardRate;
         }
-
         lastUpdateTime = block.timestamp;
         periodFinish = block.timestamp.add(DURATION);
 
@@ -201,6 +124,10 @@ contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, Reentr
 
         emit RewardAdded(_amount);
     }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }    
 
     function recoverPerezosoToken() external onlyOwnerOrDeployer {
         uint256 amountToRecover = perezoso.balanceOf(address(this));
@@ -213,29 +140,8 @@ contract PerezosoFarmingLP is TokenWrapper, RewardsDistributionRecipient, Reentr
         emit TokensRecovered(tokenAddress, tokenAmount);
     }
 
-    function setDuration(uint256 _duration) external onlyOwnerOrDeployer {
-        require(periodFinish == 0, "Cannot change duration after rewards distribution has started");
-        require(_duration > 0, "Duration must be greater than 0");
-        DURATION = _duration;
-    }
-
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }    
-
-    modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = lastTimeRewardApplicable();
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-        _;
-    }
-
     modifier onlyOwnerOrDeployer() {
         require(msg.sender == owner() || msg.sender == deployer, "Caller is not the owner or deployer");
         _;
-    }
-
+    }    
 }
--- src/TokenWrapper.sol
+++ src/TokenWrapper.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 
 abstract contract TokenWrapper {
     using SafeERC20 for IERC20;
 
-    IERC20 public perezosoToken = IERC20(0x53Ff62409B219CcAfF01042Bb2743211bB99882e);
-
+    IERC20 public stakingToken;
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
+
+    constructor(address _stakingToken) {
+        stakingToken = IERC20(_stakingToken);
+    }
 
     function totalSupply() public view virtual returns (uint256) {
         return _totalSupply;
@@ -22,14 +24,14 @@ abstract contract TokenWrapper {
     }
 
     function stake(uint256 amount) public virtual {
-        _totalSupply = _totalSupply + (amount);
-        _balances[msg.sender] = _balances[msg.sender] + (amount);
-        perezosoToken.safeTransferFrom(msg.sender, address(this), amount);
+        _totalSupply += amount;
+        _balances[msg.sender] += amount;
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
     }
 
     function withdraw(uint256 amount) public virtual {
-        _totalSupply = _totalSupply - (amount);
-        _balances[msg.sender] = _balances[msg.sender] - (amount);
-        perezosoToken.safeTransfer(msg.sender, amount);
+        _totalSupply -= amount;
+        _balances[msg.sender] -= amount;
+        stakingToken.safeTransfer(msg.sender, amount);
     }
 }
